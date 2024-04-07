70cd3ccf9f7cf19244876412ff9dba5c6114e84e
Charles Munat <charles.munat@gmail.com>
2024-04-07 13:57:02 +1200

    0.0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,19 @@
-import mdx from "@astrojs/mdx"
-import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 
-const ignore = [
-	"https://jakebaxendale.com/contact/",
-	"https://jakebaxendale.com/contact/failure/",
-	"https://jakebaxendale.com/contact/invalid-email/",
-	"https://jakebaxendale.com/contact/missing-email/",
-	"https://jakebaxendale.com/contact/missing-message/",
-	"https://jakebaxendale.com/contact/success/",
-	"https://jakebaxendale.com/cookie-policy/",
-	"https://jakebaxendale.com/feedback/",
-	"https://jakebaxendale.com/feedback/failure/",
-	"https://jakebaxendale.com/feedback/invalid-email/",
-	"https://jakebaxendale.com/feedback/missing-feedback/",
-	"https://jakebaxendale.com/feedback/success/",
-	"https://jakebaxendale.com/portfolio/",
-	"https://jakebaxendale.com/privacy-policy/",
-	"https://jakebaxendale.com/terms-of-use/",
-]
+const ignore = [];
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jakebaxendale.com/",
+	site: "https://cyberfern.com/",
 	integrations: [
 		mdx(),
 		sitemap({
-			canonicalURL: "https://jakebaxendale.com/",
+			canonicalURL: "https://cyberfern.com/",
 			filter: (page) => !ignore.includes(page),
 			lastmod: new Date(),
 		}),
 	],
-})
+});

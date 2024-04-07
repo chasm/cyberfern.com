c4653059f019741cb479cc2a7074375d6dc34558
Charles Munat <charles.munat@gmail.com>
2024-04-07 13:33:34 +1200

    chore: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+import mdx from "@astrojs/mdx"
+import { defineConfig } from "astro/config"
+
+import sitemap from "@astrojs/sitemap"
+
+const ignore = [
+	"https://jakebaxendale.com/contact/",
+	"https://jakebaxendale.com/contact/failure/",
+	"https://jakebaxendale.com/contact/invalid-email/",
+	"https://jakebaxendale.com/contact/missing-email/",
+	"https://jakebaxendale.com/contact/missing-message/",
+	"https://jakebaxendale.com/contact/success/",
+	"https://jakebaxendale.com/cookie-policy/",
+	"https://jakebaxendale.com/feedback/",
+	"https://jakebaxendale.com/feedback/failure/",
+	"https://jakebaxendale.com/feedback/invalid-email/",
+	"https://jakebaxendale.com/feedback/missing-feedback/",
+	"https://jakebaxendale.com/feedback/success/",
+	"https://jakebaxendale.com/portfolio/",
+	"https://jakebaxendale.com/privacy-policy/",
+	"https://jakebaxendale.com/terms-of-use/",
+]
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://jakebaxendale.com/",
+	integrations: [
+		mdx(),
+		sitemap({
+			canonicalURL: "https://jakebaxendale.com/",
+			filter: (page) => !ignore.includes(page),
+			lastmod: new Date(),
+		}),
+	],
+})
